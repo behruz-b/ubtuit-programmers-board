c6055e0fe17902a325399086ea3bd94439e53817
Behruz <behruz.ravshonbekovich@gmail.com>
2020-01-27 15:11:49 +0500

    ErrorHandler is added

---
+++ app/ErrorHandler.scala
@@ -0,0 +1,20 @@
+import play.api.http.HttpErrorHandler
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent._
+import javax.inject.Singleton
+
+@Singleton
+class ErrorHandler extends HttpErrorHandler {
+  def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
+    Future.successful(
+      Status(statusCode)("A client error occurred: " + message)
+    )
+  }
+
+  def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
+    Future.successful(
+      InternalServerError("A server error occurred: " + exception.getMessage)
+    )
+  }
+}
